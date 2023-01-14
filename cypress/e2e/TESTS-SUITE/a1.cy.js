1c3f1d38343990cdd3a5efe18749dd26f337c02b
CRISTIAN HERRERA <crisherreraoficial@gmail.com>
2023-01-14 18:02:03 -0300

    SUSCRIPTION H2

--- cypress/e2e/TESTS-SUITE/a1.cy.js
+++ cypress/e2e/TESTS-SUITE/a1.cy.js
@@ -32,6 +32,10 @@ describe('REPORTS - HTML-', () => {
         cy.get('.features_items > .title').should("contain","Features Items")
       });
 
+      it.only('SUSCRIPCION CHECK VISIBLE', () => {
+        cy.get('.single-widget > h2').contains("Subscription").should("be.visible")
+      });
+
         
 
 
